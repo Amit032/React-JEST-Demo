cb48ab81d90c602da15b2876a3abb5c8abc31ea6
Amit032 <032amit@gmail.com>
2019-07-25 15:59:52 +0530

    Adding Proptypes test cases to our headline component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Header from './component/header';
 import Headline from './component/headline';
 import './app.scss'
 
+const tempArr = [{
+  fName: 'Amit',
+  lName: 'Kumar',
+  email: 'amit123@gmail.com',
+  age: 24,
+  onlineSttaus: true,
+}]
+
 function App() {
   return (
     <div className="App">
       <Header />
       <section className="main">
-        <Headline header="Posts" desc="Click the button to render" />
+        <Headline header="Posts" desc="Click the button to render" tempArr={tempArr} />
       </section>
     </div>
   );
